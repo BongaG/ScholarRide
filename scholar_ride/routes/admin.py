47431c1672f51ba93112c4f9f8bbf2a7960db319
nkosingimele2005-lab <nkosingimele2005@gmail.com>
2026-03-20 16:13:58 +0200

    Updated admin route functionality

--- scholar_ride/routes/admin.py
+++ scholar_ride/routes/admin.py
@@ -87,7 +87,7 @@ def approve_user(user_id):
 
     notif = Notification(
         user_id=user.id,
-        message='✅ Your registration has been approved! Check your email for your verification code.'
+        message='✅ Your  registration has been approved' 
     )
     db.session.add(notif)
     db.session.commit()
